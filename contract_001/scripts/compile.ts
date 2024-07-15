52303a487311abb669616d33038d9abe365d09e5
M. A. <mohaliyeu@gmail.com>
2024-07-15 17:19:53 +0300

    day one challenge - learning ton contract development - compiling

--- contract_001/scripts/compile.ts
+++ contract_001/scripts/compile.ts
@@ -4,6 +4,33 @@ import {Cell} from 'ton-core';
 import {compileFunc} from '@ton-community/func-js';
 
 
+/**
+ * This TypeScript script is designed to compile FunC code for the TON blockchain using the @ton-community/func-js compiler.
+ * 
+ * Overview:
+ * - It starts by importing necessary modules such as 'fs' for file system operations, 'process' for accessing system process details,
+ *   and specific TON-related utilities like 'Cell' from 'ton-core' and 'compileFunc' from '@ton-community/func-js'.
+ * 
+ * - The script defines an asynchronous function `compileScript` which orchestrates the compilation process.
+ * 
+ * Steps performed by `compileScript`:
+ *   1. Logs the start of the compilation process.
+ *   2. Calls `compileFunc`, passing it an object with:
+ *      - `targets`: An array containing the path to the FunC source file to compile.
+ *      - `sources`: A function that reads and returns the content of the source file specified in `targets`.
+ *   3. Checks the result of the compilation:
+ *      - If there's an error (`status` is "error"), it logs the error message and exits the process with a status code of 1.
+ *      - If the compilation is successful, it proceeds to the next steps.
+ *   4. Defines a path where the compiled code artifact (in JSON format) will be saved.
+ *   5. Writes the compiled code to the specified path. The compiled code is obtained by converting the Base64 encoded BOC (Bag of Cells)
+ *      returned by the compiler into a hex string.
+ *   6. Logs the success message and the path to the saved compiled code artifact.
+ * 
+ * - Finally, it calls `compileScript` to execute the compilation process.
+ * 
+ * This script is particularly useful for developers working with TON blockchain smart contracts, providing an automated way to compile
+ * FunC code and save the compiled artifacts for deployment.
+ */
 async function compileScript() {
     console.log(
       "================================================================="
